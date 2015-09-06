02f4de1bb05bddb93364fa36a3dce04a0d09458e
Raiderium <raiderium.neo@gmail.com>
2015-09-06 17:13:41 +1000

    Package convention

--- source/raider/audio/all.d
+++
@@ -1,8 +0,0 @@
-module raider.audio.all;
-
-public import raider.audio.listener;
-public import raider.audio.medium;
-public import raider.audio.music;
-public import raider.audio.region;
-public import raider.audio.sound;
-public import raider.audio.speaker;
---
+++ source/raider/audio/package.d
@@ -0,0 +1,11 @@
+module raider.audio;
+
+public
+{
+	import raider.audio.listener;
+	import raider.audio.medium;
+	import raider.audio.music;
+	import raider.audio.region;
+	import raider.audio.sound;
+	import raider.audio.speaker;
+}
